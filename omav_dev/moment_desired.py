5ad5b024695b85085ff896742f2e81306b190fba
ars-21 <arsindia21@gmail.com>
2022-09-08 13:17:55 +0530

    Omav_Controller

--- omav_dev/moment_desired.py
+++ omav_dev/moment_desired.py
@@ -4,28 +4,62 @@ import numpy as np
 import math
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
-def moment_desired(roll_desired, pitch_desired, yaw_desired, roll, pitch, yaw , w_x_current, w_y_current, w_z_current, Inertial_Matrix, kq, kr):
-    
-    # Since angles will be given in degrees we need to convert to radians which is standard convention
-    roll_desired = roll_desired * (math.pi/180)
-    pitch_desired = pitch_desired * (math.pi/180)
-    yaw_desired = yaw_desired * (math.pi/180)
-    roll = roll * (math.pi/180)
-    pitch = pitch * (math.pi/180)
-    yaw = yaw * (math.pi/180)
+# INITIALIZING PARAMETERS USED IN CALCULATIONS
 
-    quaternion_desired = quaternion_from_euler(roll_desired,pitch_desired,yaw_desired)
-    quaternion_current = quaternion_from_euler(roll, pitch, yaw)
-    
-    q_x_desired = quaternion_desired[0]
-    q_y_desired = quaternion_desired[1]
-    q_z_desired = quaternion_desired[2]
-    q_w_desired = quaternion_desired[3]
-    q_x_current = quaternion_current[0] 
-    q_y_current = quaternion_current[1]
-    q_z_current = quaternion_current[2]
-    q_w_current = quaternion_current[3]
+# Since Equations are to be made smaller_name variables used
+# Quaternion Desired x, y, z & w
+p0 = 0
+p1 = 0
+p2 = 0
+p3 = 0
+# Quaternion Current x, y, z & w
+q0 = 0
+q1 = 0
+q2 = 0
+q3 = 0
+# Quaternion Error x, y, z & w
+q_w_error = 0
+q_x_error = 0
+q_y_error = 0
+q_z_error = 0
+# Sign of Quaternion Error w
+sign_q_w_error = 0
+# 3*1 Matrix of the vector part of Quaternion Error
+q_v_error = np.zeros((1, 3))
+# 3*1 Matrix of Desired Angular Velocity
+w_desired = np.zeros((1, 3))
+# 3*1 Matrix of the Current Angular Velocity
+w_current= np.zeros((1, 3))
+# 3*1 Matrix of Error in Angular Velocity
+w_error = np.zeros((1, 3))
 
+def moment_desired(q_x_desired, q_y_desired, q_z_desired, q_w_desired, q_x_current, q_y_current, q_z_current, q_w_current, w_x_current, w_y_current, w_z_current, Inertial_Matrix, kq, kr, flag):
+    
+    # Global variables are declared to avoid their values resetting to 0
+    global p0, p1, p2, p3, q0, q1, q2, q3, q_w_error, q_x_error, q_y_error, q_z_error, sign_q_w_error
+    global q_v_error, w_desired, w_current, w_error
+
+    # For Initial Start
+    if(flag==0):
+        p0 = 0
+        p1 = 0
+        p2 = 0
+        p3 = 0
+        q0 = 0
+        q1 = 0
+        q2 = 0
+        q3 = 0
+        q_w_error = 0
+        q_x_error = 0
+        q_y_error = 0
+        q_z_error = 0
+        sign_q_w_error = 0
+        q_v_error = np.zeros((1, 3))
+        w_desired = np.zeros((1, 3))
+        w_current= np.zeros((1, 3))
+        w_error = np.zeros((1, 3))
+
+    # Assigning values to smaller_name variables
     p0 = q_w_desired
     p1 = q_x_desired
     p2 = q_y_desired
@@ -36,24 +70,53 @@ def moment_desired(roll_desired, pitch_desired, yaw_desired, roll, pitch, yaw , 
     q2 = -q_y_current
     q3 = -q_z_current
 
-
+    """
+    Quaternion Error Equations : 
+        qerr = qdes,IB ⊗ q̂IB = | qw,err |
+                               | qv,err | , which is a 4*1 Matrix
+        where, ⊗ = Kronecker product
+               qerr = Quaternion Error, which is a 4*1 Matrix
+               qdes,IB = Desired Quaternion Orientation of the Body Frame to Intertial Frame, which is a 4*1 Matrix
+               q̂IB = Current Quaternion Orientation of the Body Frame to Intertial Frame, which is a 4*1 Matrix
+               qw,err = real part of Quaternion Error
+               qv,err = vector part of Quaternion Error, which is a 3*1 Matrix
+    """
     q_w_error = (p0*q0 - p1*q1 - p2*q2 - p3*q3)
     q_x_error = (p0*q1 + p1*q0 + p2*q3 - p3*q2)
     q_y_error = (p0*q2 - p1*q3 + p2*q0 + p3*q1)
     q_z_error = (p0*q3 + p1*q2 - p2*q1 + p3*q0)
 
+    # We Require only the sign of q_w_error for further Calculation
     if(q_w_error < 0):
-        sign_q_error = -1
+        sign_q_w_error = -1
     elif(q_w_error > 0):
-        sign_q_error = 1
+        sign_q_w_error = 1
 
-    q_v_error = [q_x_error, q_y_error, q_z_error]
+    # Initializing the 3*1 Matrix of the vector part of Quaternion Error for Further Calculations
+    q_v_error = np.array([q_x_error, q_y_error, q_z_error])
 
-    w_desired = [kq * sign_q_error *  q_x_error, kq * sign_q_error *  q_y_error, kq * sign_q_error *  q_z_error]
+    """
+    The desired body rate ωdes (desired angular velocity) is generated from the vector part of the quaternion error qv,err as :
+        ωdes = kq.sign(qw,err).qv,err
+        where, . = means scalar multiplication of a vector
+               ωdes = desired body rate (desired angular velocity), which is a 3*1 Matrix
+               kq = tuning parameter, which is a scalar
+               sign(qw,err) = the sign of the real part of the quaternion error is used to avoid the unwinding phenomena, which is a scalar
+               qv,err = vector part of Quaternion Error, which is a 3*1 Matrix
+    """
+    w_desired = (kq * sign_q_w_error * q_v_error)
 
-    w_current = [w_x_current, w_y_current,w_z_current]
+    # Initializing the 3*1 Matrix of the Current Angular Velocity
+    w_current = np.array([w_x_current, w_y_current,w_z_current])
 
+    # To Find Error in Angular Velocity which is a 3*1 Matrix
     w_error = np.array(w_desired) - np.array(w_current)
+
+    """
+    The desired moments Mdes are computed as follows :
+        Mdes = kr.(ωdes - ω̂ ) - (roff × BFdes) + (ω̂  × (J.ω̂ ))
+    """
+
     q_intermediate_1 = (kr * w_error)
     q_intermediate_2_1 = np.array(np.matmul(Inertial_Matrix, w_current))
     q_intermediate_2_1 = np.cross(w_current, q_intermediate_2_1)
--- omav_dev/test.py
+++ omav_dev/test.py
@@ -1,6 +1,18 @@
 import numpy as np
 import math
 
+a1 = np.array([1, 2, 3])
+print(a1)
+print(type(a1))
+
+a2 = np.array([2, 3, 4])
+
+a3 = (1*2*a2)
+print(a3)
+print(type(a3))
+print(a3[0])
+
+
 q_v_error1 = np.matrix([[1], [2], [3]])
 q_v_error2 = np.matrix([[3], [2], [1]])
 
