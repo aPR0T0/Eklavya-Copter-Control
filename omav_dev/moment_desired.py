2de542d0ed50930468639af32db0b358f8011497
ars-21 <arsindia21@gmail.com>
2022-09-08 12:12:15 +0530

    Omav_Controller

--- omav_dev/moment_desired.py
+++ omav_dev/moment_desired.py
@@ -1,29 +1,30 @@
 #! /usr/bin/env python3
 import rospy
-import time
-import message_filters
-from std_msgs.msg import Float64, Float64MultiArray
-from mav_msgs.msg import Actuators  
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import numpy as np
 import math
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
-
-def moment_desired(roll_desired, pitch_desired, yaw_desired, q_x_current, q_y_current, q_z_current, q_w_current, w_x_current, w_y_current, w_z_current):
-
+def moment_desired(roll_desired, pitch_desired, yaw_desired, roll, pitch, yaw , w_x_current, w_y_current, w_z_current, Inertial_Matrix, kq, kr):
+    
     # Since angles will be given in degrees we need to convert to radians which is standard convention
     roll_desired = roll_desired * (math.pi/180)
     pitch_desired = pitch_desired * (math.pi/180)
     yaw_desired = yaw_desired * (math.pi/180)
+    roll = roll * (math.pi/180)
+    pitch = pitch * (math.pi/180)
+    yaw = yaw * (math.pi/180)
 
     quaternion_desired = quaternion_from_euler(roll_desired,pitch_desired,yaw_desired)
-
+    quaternion_current = quaternion_from_euler(roll, pitch, yaw)
+    
     q_x_desired = quaternion_desired[0]
     q_y_desired = quaternion_desired[1]
     q_z_desired = quaternion_desired[2]
     q_w_desired = quaternion_desired[3]
+    q_x_current = quaternion_current[0] 
+    q_y_current = quaternion_current[1]
+    q_z_current = quaternion_current[2]
+    q_w_current = quaternion_current[3]
 
     p0 = q_w_desired
     p1 = q_x_desired
@@ -46,25 +47,18 @@ def moment_desired(roll_desired, pitch_desired, yaw_desired, q_x_current, q_y_cu
     elif(q_w_error > 0):
         sign_q_error = 1
 
-    q_v_error = np.matrix([[q_x_error], [q_y_error], [q_z_error]])
+    q_v_error = [q_x_error, q_y_error, q_z_error]
 
-    w_desired = (kq * sign_q_error * q_v_error)
+    w_desired = [kq * sign_q_error *  q_x_error, kq * sign_q_error *  q_y_error, kq * sign_q_error *  q_z_error]
 
-    w_current = np.matrix([[w_x_current], [w_y_current], [w_z_current]])
+    w_current = [w_x_current, w_y_current,w_z_current]
 
-    w_error = w_desired - w_current
-    
+    w_error = np.array(w_desired) - np.array(w_current)
     q_intermediate_1 = (kr * w_error)
-
-    q_intermediate_2_1 = np.matmul(J, w_current)
-
+    q_intermediate_2_1 = np.array(np.matmul(Inertial_Matrix, w_current))
     q_intermediate_2_1 = np.cross(w_current, q_intermediate_2_1)
+    
+    M_des =  np.zeros([3,1])
+    M_des = q_intermediate_1 + q_intermediate_2_1
 
-    M_desired = q_intermediate_1 + q_intermediate_2_1
-
-    return(M_desired)
-
-
-
-
-
+    return M_des
